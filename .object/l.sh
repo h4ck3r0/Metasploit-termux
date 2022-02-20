e79fe40fddef58b0c5a54693cde6da3eacd38de4
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2022-02-20 21:50:47 +0530

    Create l.sh

---
+++ .object/l.sh
@@ -0,0 +1,77 @@
+clear
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED} ███████ ▓█████▄▄▄█████▓ █    ██  ██▓███  " 
+echo -e "${RED}▒██    ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒" 
+echo -e "${RED}░ ▓██▄   ▒███  ▒ ▓██░ ▒░▓██  ▒██░▓██░ ██▓▒" 
+echo -e "${RED}  ▒   ██▒▒▓█  ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒" 
+echo -e "${RED}▒██████▒▒░▒████▒ ▒██▒ ░ ▒▒█████▓ ▒██▒ ░  ░" 
+echo -e "${RED}▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░" 
+echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ ░▒ ░     " 
+echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░       " 
+echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
+
+cwd=$(pwd)
+msfvar=6.0.27
+msfpath='/data/data/com.termux/files/home'
+
+echo -e "\033[92m"
+center "INSTALLING REQUIREED PACKAGES"
+echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
+
+apt update && apt upgrade -y
+
+apt install -y binutils libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make ruby libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
+# Many phones are claiming libxml2 not found error
+ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
+
+echo -e "\e[34mPACKAGES INSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[0m"
+sleep 5.0
+clear
+echo -e "\033[92m"
+center "INSTALLING  METASPLOIT"
+echo -e "\e[34mINSTALLING METASPLOIT....\e[0m"
+
+cd $msfpath
+curl -LO https://github.com/rapid7/metasploit-framework/archive/refs/tags/$msfvar.tar.gz
+
+tar -xf $msfpath/$msfvar.tar.gz
+mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
+cd $msfpath/metasploit-framework
+echo -e "\033[92m"
+center "Working On Some Fixes .....\e[0m"
+apt remove -y ruby
+cp -r ~/Metasploit-termux/.object/ruby.deb $loc
+cd $loc
+apt install -y ./ruby.deb 
+apt-mark hold ruby
+cd $loc/metasploit-framework 
+gem install bundler
+bundle config set force_ruby_platform true
+bundle install
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+bundle config build.nokogiri --use-system-libraries
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt >/dev/null 2>&1
+bash fix-ruby-bigdecimal.sh.txt
+clear
+echo -e "\033[92m"
+center "Creating Postgresql Database\e[0m"
+mkdir -p $PREFIX/var/lib/postgresql >/dev/null 2>&1
+initdb $PREFIX/var/lib/postgresql 
+pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+cd $loc
+rm $ver.tar.gz 
+rm ruby.deb 
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
+echo -e "\e[34mTO START METASPLOIT TYPE (./msfconsole) INSIDE METASPLOIT FRAMEWORK\e[0m"
+
+cd $loc/metasploit-framework
+clear
+./msfconsole

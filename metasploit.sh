643f313f8d47feeea9f0f20d6465c9448884f7b6
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2022-03-24 09:10:18 +0530

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -21,7 +21,7 @@ echo '
   .---------------------------------------.                    
                                                                                                            
 ' | lolcat
- echo""
+ echo ""
  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92m Youtube.com/c/H4Ck3R0\e[0m"
 
                   }

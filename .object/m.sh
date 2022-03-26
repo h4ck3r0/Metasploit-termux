81320a665a1667f88299c24f203115d8157d549e
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2022-03-26 11:04:20 +0530

    Update m.sh

--- .object/m.sh
+++ .object/m.sh
@@ -16,142 +16,111 @@ echo -e "${RED}░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ �
 echo -e "${RED}░  ░  ░     ░    ░       ░░░ ░ ░ ░░       " 
 echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   " 
 sleep 5.0
-if [ -d $HOME/metasploit-framework ];
-then
-echo -e "\033[92mCHEKING OLD METASPLOIT"
-echo -e "\e[34mREMOVING METASPLOIT.....WAIT\e[0m"
+
+# Install gnupg required to sign repository
+pkg install -y gnupg
+
+# Sign gushmazuko repository
+curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
+
+# Add gushmazuko repository to install ruby 2.7.2 version
+echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
+
+# Set low priority for all gushmazuko repository (for security purposes)
+# Set highest priority for ruby package from gushmazuko repository
+echo '## Set low priority for all gushmazuko repository (for security purposes)
+Package: *
+Pin: release gushmazuko
+Pin-Priority: 100
+## Set highest priority for ruby package from gushmazuko repository
+Package: ruby
+Pin: release gushmazuko
+Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
+
+# Purge installed ruby
+apt purge ruby -y
+rm -fr $PREFIX/lib/ruby/gems
+echo -e "\033[92mINSTALLING REQUIREED PACKAGES"
+sleep 5.0
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew" --allow-change-held-packages
+echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
+sleeep 5.0
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+
+echo -e "\e[34mPACKAGES INSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[0m"
+sleep 5.0
+
+echo -e "\033[92mFix ruby BigDecimal..."
+echo ""
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+
+echo -e  "\033[92mCHEKING OLD METASPLOIT"
 find . -type d -name "metasploit-*" -exec rm -rf "{}" \; >/dev/null 
-rm -rf metasploit-framework
 sleep 4.0
+echo -e "\e[34mREMOVING METASPLOIT.....WAIT\e[0m"
+rm -rf $HOME/metasploit-framework
 echo -e "\e[34mREMOVED METASPLOIT SUCCESSFULLY.....[\e[92m✓\e[34m]\e[0m"
 sleep 4.0
-cwd=$(pwd)
-else
 echo
-fi
-if [[ $arc = "arm" ]];
-then
-msfvar=6.1.35
-msfpath='/data/data/com.termux/files/home'
-
-echo -e "\033[92mINSTALLING REQUIREED PACKAGES"
-echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
-
-apt update && apt upgrade -y
-
-pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew" --allow-change-held-packages
-apt --fix-broken install
-# Many phones are claiming libxml2 not found error
-ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
-
-echo -e "\e[34mPACKAGES INSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[0m"
-sleep 5.0
 clear
-echo -e "\033[92mINSTALLING  METASPLOIT"
 echo -e "\e[34mINSTALLING METASPLOIT....\e[0m"
-
-cd $msfpath
-curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
-
-tar -xf $msfpath/$msfvar.tar.gz
-mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
-cd $msfpath/metasploit-framework
-echo -e "\033[92mWorking On Some Fixes .....\e[0m"
-cp -r ~/Metasploit-termux/.object/ruby1.deb $HOME
+sleep 5.0
 cd $HOME
-apt-mark hold ruby
-wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt >/dev/null 2>&1
-bash fix-ruby-bigdecimal.sh.txt
-cd $HOME/metasploit-framework 
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1
+clear
+echo -e "\033[92mWorking On Some Fixes .....\e[0m"
+sleep 5.0
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+
+echo 
+
+
+export MSF_FIX="spec.add_runtime_dependency 'net-smtp'"
+sed -i "146i \  $MSF_FIX" metasploit-framework.gemspec
+sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
+
 gem install bundler
-bundle config set force_ruby_platform true
-bundle install
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+
 gem install actionpack
 bundle update activesupport
 bundle update --bundler
 bundle install -j$(nproc --all)
-bundle config build.nokogiri --use-system-libraries
-
-clear
-echo -e "\033[92mCreating Postgresql Database\e[0m"
-mkdir -p $PREFIX/var/lib/postgresql >/dev/null 2>&1
-initdb $PREFIX/var/lib/postgresql 
-pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
-cd $HOME
-rm $ver.tar.gz 
-rm ruby1.deb 
-bundle install
-echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
-echo -e "\e[34mTO START METASPLOIT TYPE (./msfconsole) INSIDE METASPLOIT FRAMEWORK\e[0m"
-sleep 5.0
-cd $HOME/metasploit-framework
-clear
-./msfconsole
-
-elif [[ $arc = "aarch64" ]];
-then
-msfvar=6.1.35
-msfpath='/data/data/com.termux/files/home'
-
-echo -e "\033[92mINSTALLING REQUIREED PACKAGES"
-echo -e "\e[34mPACKAGES BEING INSTALLED WAIT....\e[0m"
-
-apt update && apt upgrade -y
-
-pkg install -y python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew" --allow-change-held-packages
-apt --fix-broken install
-# Many phones are claiming libxml2 not found error
-ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
-
-echo -e "\e[34mPACKAGES INSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[0m"
-sleep 5.0
-clear
-echo -e "\033[92mINSTALLING  METASPLOIT"
-echo -e "\e[34mINSTALLING METASPLOIT....\e[0m"
-
-cd $msfpath
-curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
-
-tar -xf $msfpath/$msfvar.tar.gz
-mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
-cd $msfpath/metasploit-framework
-echo -e "\033[92mWorking On Some Fixes .....\e[0m"
-cp -r ~/Metasploit-termux/.object/ruby.deb $HOME
-cd $HOME
-apt-mark hold ruby
-sleep 5.0
-wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt >/dev/null 2>&1
-bash fix-ruby-bigdecimal.sh.txt
-cd $HOME/metasploit-framework 
-cd $HOME/metasploit-framework
-sleep 3.0
-gem install bundler
-bundle config set force_ruby_platform true
-sleep 3.0
-bundle install
-sleep 2.0
-gem install actionpack
-bundle update activesupport
-bundle update --bundler
-bundle install -j$(nproc --all)
-bundle config build.nokogiri --use-system-libraries
-echo -e "\033[92mCreating Postgresql Database\e[0m"
-mkdir -p $PREFIX/var/lib/postgresql >/dev/null 2>&1
-initdb $PREFIX/var/lib/postgresql 
-pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
-bundle install
-cd $HOME
-rm $ver.tar.gz 
-rm ruby.deb 
-else
-echo
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
 fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
 
-echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
-echo -e "\e[34mTO START METASPLOIT TYPE (./msfconsole) INSIDE METASPLOIT FRAMEWORK\e[0m"
+echo
+echo -e "\033[32m Still Fixing\033[0m"
+
+sed -i '355 s/::Exception, //' $PREFIX/bin/msfvenom
+sed -i '481, 483 {s/^/#/}' $PREFIX/bin/msfvenom
+sed -Ei "s/(\^\\\c\s+)/(\^\\\C-\\\s)/" /data/data/com.termux/files/home/metasploit-framework/lib/msf/core/exploit/remote/vim_soap.rb
+sed -i '86 {s/^/#/};96 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/logging-2.3.0/lib/logging/diagnostic_context.rb
+rm -rf /data/data/com.termux/files/usr/bin/msfvenom
+sed -i '13,15 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb; sed -i '14 {s/^/#/}' /data/data/com.termux/files/usr/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb;
+
+cd;cd metasploit-framework;ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+echo -e "\033[92mCreating Postgresql Database\e[0m"
 sleep 5.0
-cd $HOME/metasploit-framework
-clear
-./msfconsole
+mkdir -p $PREFIX/var/lib/postgresql >/dev/null 2>&1
+initdb $PREFIX/var/lib/postgresql 
+pg_ctl -D /data/data/com.termux/files/usr/var/lib/postgresql -l logfile start
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
+echo -e "\033[32m  type msfconsole to run the metasploit\033[0m"
